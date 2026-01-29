7159063f45f1dd122cc4209d4606750c7cc3311f
pheereel <farrrrell14@gmail.com>
2026-01-29 08:33:34 +0700

    jeki anjing

---
+++ script.js
@@ -0,0 +1,16 @@
+const lagu = document.querySelector('#mySong')
+const button = document.querySelector('#ctrlIcon')
+
+function playmusic () {
+    if (song.paused) {
+      song.play();
+      // Ganti ikon ke Pause
+      icon.classList.remove("fa-play");
+      icon.classList.add("fa-pause");
+    } else {
+      song.pause();
+      // Ganti ikon balik ke Play
+      icon.classList.remove("fa-pause");
+      icon.classList.add("fa-play");
+    }
+}
