71deeb9f36dae7cc72af81106ee343caa4663b84
pheereel <farrrrell14@gmail.com>
2026-01-29 09:12:49 +0700

    m

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const lagu = document.querySelector('#mySong')
+const lagu = new Audio('/audio/tulus.mp3')
 const button = document.querySelector('#ctrlIcon')
 
 function playmusic() {
-    if (song.paused) {
-      song.play();
+    if (lagu.paused) {
+      lagu.play();
       // Ganti ikon ke Pause
       icon.classList.remove("fa-play");
       icon.classList.add("fa-pause");
     } else {
-      song.pause();
+      lagu.pause();
       // Ganti ikon balik ke Play
       icon.classList.remove("fa-pause");
       icon.classList.add("fa-play");
     }
 }
 
-lagu.addEventListener("click", playmusic)
+button.addEventListener("click", playmusic)
